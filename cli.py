489e2843da1d6fd914909bae9fc85b6a05dce319
Chaos Klopp <klopp@Chaoss-MBP.attlocal.net>
2025-03-28 19:34:08 -0700

    added cli.py skeleton

---
+++ cli.py
@@ -0,0 +1,39 @@
+import argparse
+
+
+class CLIArgs:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description="Womb Raider Pipeline CLI")
+        self._add_arguments()
+        self.args = self.parser.parse_args()
+
+
+    def _add_arguments(self):
+        self.parser.add_argument(
+            "--convert-fastq",
+            action="store_true",
+            help="Convert .sra to .fastq after download",
+        )
+
+        self.parser.add_argument(
+            "--batch-size",
+            type=int,
+            default=5,
+            help="Number of parallel downloads",
+        )
+
+        self.parser.add_argument(
+            "--max-retries",
+            type=int,
+            default=5,
+            help="Maximum retries for failed downloads",
+        )
+
+        self.parser.add_argument(
+            "--use-s3",
+            action="store_true",
+            help="Enable S3 upload after download",
+        )
+
+    def get_args(self):
+        return self.args

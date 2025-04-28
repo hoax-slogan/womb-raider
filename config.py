69be0cf9876dd0d6443366032ed5693f15837124
Chaos Klopp <klopp@Chaoss-MBP.attlocal.net>
2025-04-28 14:49:43 -0700

    added split_fastq directory to config.yaml + config.py

--- config.py
+++ config.py
@@ -61,6 +61,7 @@ class Config:
         self.sra_output_dir = self.data_dir / subdirs["output"]
         self.logs_dir = self.data_dir / subdirs["logs"]
         self.fastq_dir = self.data_dir / subdirs["fastq"]
+        self.split_fastq_dir = self.data_dir / subdirs["split_fastq"]
         self.star_dir = self.data_dir / subdirs["star"]
 
         logs = self.config["logs"]
@@ -77,6 +78,7 @@ class Config:
             self.sra_lists_dir,
             self.sra_output_dir,
             self.fastq_dir,
+            self.split_fastq_dir,
             self.star_genome_dir,
             self.star_output_dir,
             self.csv_log_dir,
